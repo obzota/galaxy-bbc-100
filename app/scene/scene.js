1bc8f54e36f2cd761af9cb20f1231f1a3983684c
obzota <rey.rjf@gmail.com>
2017-01-16 15:27:10 +0100

    rework on style

--- app/scene/scene.js
+++ app/scene/scene.js
@@ -6,10 +6,10 @@ function Scene(movies, critics) {
 	this.movie = null;
 	this.size = 1000; // in pixels
 	this.scaling = 1;
-this.movieIsSelected = false;
+	this.movieIsSelected = false;
 	this.scale = d3.scaleLinear()
-		.domain([-1,1])
-		.range([10,990]);
+	.domain([-1,1])
+	.range([10,990]);
 
 	givePosition(this.movies);
 }
@@ -24,48 +24,52 @@ Scene.prototype.drawGalaxy = function() {
 	//selectGenre = that.drawColorButtons();
 
 	var solarSystems = this.selectMovies(movies); // = d3.select('#movies').selectAll('circle').data(movies, keyFunc)
-	var enter = solarSystems.enter().append('circle'); // append new circles for 
-movies
+	var enter = solarSystems.enter().append('circle'); // append new circles for
+	movies
 	enter
-			.classed('movie', true)
-		    .style('fill', function(movie) {return movie.color(selectGenre)}) 
-			//TODO: pass the value of the radio button of the matrix
-		    .attr('r', 3) // TODO: compute radius/choose data for radius ?
-		    .attr('cx', function(movie) {return that.scale(movie.pos().farX)})
-		    .attr('cy', function(movie) {return that.scale(movie.pos().farY)});
+	.classed('movie', true)
+	.style('fill', function(movie) {return movie.color(selectGenre)})
+	//TODO: pass the value of the radio button of the matrix
+    .attr('r', 3) // TODO: compute radius/choose data for radius ?
+    .attr('cx', function(movie) {return that.scale(movie.pos().farX)})
+    .attr('cy', function(movie) {return that.scale(movie.pos().farY)});
 
-	enter.merge(solarSystems)
-		  .on('click', function(movie) {that.drawSystem(movie)})
-			.on('mouseenter', function(movie) {that.displayMovieInfo(movie)})
-			.on('mouseleave', function(movie) {that.hideMovieInfo()})
-		    .attr('data-title', function(movie) {return movie.title})
-		.transition().duration(1000)
-			.attr('cx', function(movie) {return that.scale(movie.pos().x)})
-		    .attr('cy', function(movie) {return that.scale(movie.pos().y)});
+    enter.merge(solarSystems)
+    .on('click', function(movie) {that.drawSystem(movie)})
+    .on('mouseenter', function(movie) {that.displayMovieInfo(movie)})
+    .on('mouseleave', function(movie) {that.hideMovieInfo()})
+    .attr('data-title', function(movie) {return movie.title})
+    .transition().duration(1000)
+    .attr('cx', function(movie) {return that.scale(movie.pos().x)})
+    .attr('cy', function(movie) {return that.scale(movie.pos().y)});
 }
 
 Scene.prototype.drawSystem = function(movie) {
 	this.movieIsSelected = true;
 	this.movie = movie;
-Scene.prototype.displayMovieInfo(movie);
+	Scene.prototype.displayMovieInfo(movie);
+
 	var that = this;
 	var solarSystems = this.selectMovies([movie]);
+
 	solarSystems
-		.transition()
-		.duration(1000)
-		.attr('cx', this.scale(0))
-		.attr('cy', this.scale(0));
+	.transition()
+	.duration(1000)
+	.attr('cx', this.scale(0))
+	.attr('cy', this.scale(0));
+
 	solarSystems
-		.exit()
-		.transition()
-		.duration(1000)
-	    .attr('cx', function(movie) {return that.scale(movie.pos().farX)})
-	    .attr('cy', function(movie) {return that.scale(movie.pos().farY)})
-	    .attr('r', 0)
-		.remove();
+	.exit()
+	.transition()
+	.duration(1000)
+	.attr('cx', function(movie) {return that.scale(movie.pos().farX)})
+	.attr('cy', function(movie) {return that.scale(movie.pos().farY)})
+	.attr('r', 0)
+	.remove();
+
 	solarSystems
-		.style('fill', '#000')
-		.on('click', function() {that.drawGalaxy()});
+	.style('fill', '#000')
+	.on('click', function() {that.drawGalaxy()});
 };
 
 Scene.prototype.selectMovies = function(data) {
@@ -77,13 +81,13 @@ Scene.prototype.selectMovies = function(data) {
 
 Scene.prototype.zoomIn = function() {
 	if (this.movie) {return}
-	this.scaling = 1.1 * this.scaling;
+		this.scaling = 1.1 * this.scaling;
 	this.scale.domain([-1/this.scaling, 1/this.scaling]);
 };
 
 Scene.prototype.zoomOut = function() {
 	if (this.movie) {return}
-	this.scaling = 0.9 * this.scaling;
+		this.scaling = 0.9 * this.scaling;
 	this.scale.domain([-1/this.scaling, 1/this.scaling]);
 }
 
@@ -101,63 +105,62 @@ Scene.prototype.resize = function(width, heigth) {
 
 Scene.prototype.displayMovieInfo = function(movie)
 {
-		var template = $("#template")[0].innerHTML;
-		var output = Mustache.render(template, movie);
-		$("#sidebar")[0].innerHTML = output;
+	/*
+		render movie info
+	*/
+	var template = $("#template")[0].innerHTML;
+	var output = Mustache.render(template, movie);
+	$("#movie_info")[0].innerHTML = output;
 
-	var data = movie.histogram;
+	/*
+		render movie rating histogram
+	*/
+	var data = [0,0,0,0,0,0,0,0,0,0,0];
+	movie.histogram.map((bucket) => (data[bucket[0]] = bucket.length));
+
 	var width = 100,
-    height = 100;
+	height = 100;
 
-	
-var x = d3.scaleOrdinal()
-    .range([0, width], .1);
-	
-var y = d3.scaleLinear()
-    .range([height, 0]);
+	var y = d3.scaleLinear()
+	.range([height, 0]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+	var chart = d3.select(".chart")
+	.attr("width", width)
+	.attr("height", height);
 
-x.domain(data.map(function(d) { return d[0]; }));
-  y.domain([0, d3.max(data, function(d) { return d.length; })]);
-  
-  var barWidth = width / data.length;
+	y.domain([0, d3.max(data)]);
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + 
-",0)"; })
+	var barWidth = width / data.length;
+
+	var bar = chart.selectAll("g").data(data);
+	debugger;
+	bar
+	.enter().append("g").merge(bar)
+	.attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; })
 	.append("rect")
-      .attr("y", function(d) { return y(d.length); })
-      .attr("height", function(d) { return height - y(d.length); })
-      .attr("width", barWidth - 1);
+	.attr("y", function(d) { return y(d); })
+	.attr("height", function(d) { return height - y(d); })
+	.attr("width", barWidth - 1);
 
-  bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return y(d.length) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d.length; });
+	// bar.append("text")
+	// .attr("x", barWidth / 2)
+	// .attr("y", function(d) { return y(d) + 3; })
+	// .attr("dy", ".75em")
+	// .text(function(d) { return d; });
 
-var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, 100]);
+	var x = d3.scaleLinear()
+	.domain([0, d3.max(data)])
+	.range([0, 100]);
+
+	$("#svgHisto").show();
 }
-function type(d) {
-  d.value = +d.length; // coerce to number
-  return d;
-}
-	
-
 
 Scene.prototype.hideMovieInfo = function()
-{	
+{
 	if(!this.movieIsSelected)
 	{
-		$("#sidebar")[0].innerHTML = null;
-		$("#svgHisto")[0].innerHTML = null;
+		$("#movie_info")[0].innerHTML = null;
+		$("#svgHisto").hide();
 	}
 }
 
@@ -168,24 +171,24 @@ Scene.prototype.drawColorButtons = function() {
 	var body = d3.select("body")
 	var form = body.append('form');
 	form.append('input')
-		.attr('type', 'radio')
-		.attr('value', 'Genre')
-		.attr('name', 'toggle')
-		.on('click', function () {
-				genreSelected = true;
-		});
-		form.append('label')
-				.html('Genre');
+	.attr('type', 'radio')
+	.attr('value', 'Genre')
+	.attr('name', 'toggle')
+	.on('click', function () {
+		genreSelected = true;
+	});
+	form.append('label')
+	.html('Genre');
 
-		form.append('input')
-				.attr('type', 'radio')
-				.attr('value', 'Nat')
-				.attr('name', 'toggle')
-				.on('click', function () {
-						genreSelected = false;
-		});
-		form.append('label')
-			.html('Nationality');
+	form.append('input')
+	.attr('type', 'radio')
+	.attr('value', 'Nat')
+	.attr('name', 'toggle')
+	.on('click', function () {
+		genreSelected = false;
+	});
+	form.append('label')
+	.html('Nationality');
 
-		return genreSelected;
+	return genreSelected;
 }
