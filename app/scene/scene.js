29d94ed314f953ec8afdcb092dd011ad55320e05
Jade <jade0@hotmail.fr>
2017-01-18 09:46:17 +0100

    changement couleur orbite et rank mieux

--- app/scene/scene.js
+++ app/scene/scene.js
@@ -297,7 +297,7 @@ Scene.prototype.drawOrbiteCritics = function() {
 Scene.prototype.drawOrbite = function(radius)
 {
 	d3.select('#criticsOrbite').append('circle')
-			.style('stroke', 'lightgray')
+			.style('stroke', 'DarkGrey ')
 			.style('fill', 'transparent')
 		.attr('r', radius)
 		.attr('cx', this.scale(0))
